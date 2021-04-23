0276d961e92b986a35ea2f2de3deb14a6e15274c
Scott Wang <scottwwc@hotmail.com>
2021-04-23 17:23:22 +0800

    Update init.el

--- init.el
+++ init.el
@@ -10,9 +10,14 @@
 (require 'init-base)
 (require 'init-elpa)
 (require 'init-ui)
+(require 'init-org)
 
 ;; package
 (require 'init-pkg)
 (require 'init-company)
+(require 'init-ivy)
 (require 'init-evil)
 (require 'init-evil-leader)
+
+;; after require package
+(require 'init-kbd)
